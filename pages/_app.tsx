dae3402b68eab349e30330fa47c366fb1182028c
gytks4 <gytks4@naver.com>
2021-07-06 03:53:16 +0900

    dev: eslint 수정, 자동저장

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const a = 1;
+  const b = 2;
+  const c = {
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+  const d = {
+    a,
+    b,
+    c,
+  };
+  const e = { a: 1, b: 2, c: 3 };
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
