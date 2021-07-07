a0ff818c715adfcdb95a166cb320c1b8d0f05bbd
Danna Lee <daeunlee0720@gmail.com>
2021-07-08 03:26:06 +0900

    feat: index.tsx에 헤더 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Title from 'components/home/Title';
 import React from 'react';
 
+import Header from '../components/common/header';
+
 function Home() {
   return (
     <>
+      <Header />
       <Title />
       <div>안녕하세요.</div>
     </>
