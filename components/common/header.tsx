01d9a24b0a3479117498f587612265613e156ccf
Danna Lee <daeunlee0720@gmail.com>
2021-07-08 03:27:49 +0900

    feat: 로그인 전/후 분기처리

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,23 +1,29 @@
 import Styled from '@emotion/styled';
 import React from 'react';
 
-const header: React.FC = () => {
+function header() {
+  const isLoggedIn = true;
+
   return (
     <HeaderWrap>
-      <img className="logo" src="" alt=""></img>
+      <img className="logo" src="/assets/icons/headerLogo.svg" alt=""></img>
       <div className="user">
-        <div className="user__anonymous">
-          <p className="user__anonymous--login">Log in</p>
-          <p className="user__anonymous--signUp">Sign up</p>
-        </div>
-        <div className="user__profile">
-          <img className="user__profile--picture" src="" alt=""></img>
-          <p className="user__profile--name">Name</p>
-        </div>
+        {isLoggedIn ? (
+          <div className="user__profile">
+            <p className="user__profile--logout">Log out</p>
+            <img className="user__profile--picture" src="" alt=""></img>
+            <p className="user__profile--name">Daniel</p>
+          </div>
+        ) : (
+          <div className="user__anonymous">
+            <p className="user__anonymous--login">Log in</p>
+            <p className="user__anonymous--signUp">Sign up</p>
+          </div>
+        )}
       </div>
     </HeaderWrap>
   );
-};
+}
 
 const HeaderWrap = Styled.div`
     .logo {
