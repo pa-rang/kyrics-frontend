29146d13db3084d78d55c753a0ad2aa7f445a425
Danna Lee <daeunlee0720@gmail.com>
2021-07-08 03:28:07 +0900

    style: 헤더 스타일 적용
    추후에 px -> rem 변경 예정

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,4 +1,4 @@
-import Styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import React from 'react';
 
 function header() {
@@ -25,36 +25,77 @@ function header() {
   );
 }
 
-const HeaderWrap = Styled.div`
-    .logo {
+const HeaderWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 90px;
 
+  .logo {
+    margin-left: 140px;
+    width: 176px;
+    height: 31px;
+  }
+
+  .user {
+    &__profile {
+      display: flex;
+      margin-right: 55px;
+
+      &--logout {
+        margin-right: 40px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #9d9d9d;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
+      }
+
+      &--picture {
+        border-radius: 14px;
+        box-shadow: 3px 3px 7px 4px rgba(98, 98, 98, 0.12);
+        background-color: #6465f4;
+        width: 28px;
+        height: 28px;
+        overflow: hidden;
+      }
+
+      &--name {
+        margin-left: 9px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #6465f4;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
+      }
     }
 
-    .user {
-        &__anonymous {
+    &__anonymous {
+      display: flex;
+      margin-right: 140px;
 
-            &--login {
+      &--login {
+        line-height: 27px;
+        white-space: nowrap;
+        color: #9d9d9d;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
+      }
 
-            }
-
-            &--signUp {
-
-            }
-        }
-        
-
-        &__profile{
-            
-            &--picture{
-
-            }
-
-            &--name{
-
-            }
-        }
-
+      &--signUp {
+        margin-left: 40px;
+        line-height: 27px;
+        white-space: nowrap;
+        color: #6465f4;
+        font-size: 20px;
+        font-weight: bold;
+        font-style: normal;
+      }
     }
+  }
 `;
 
 export default header;
