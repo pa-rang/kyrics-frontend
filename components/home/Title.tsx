4e8460727f3ab912c6bedc83a25a6eb032075cd9
Danna Lee <daeunlee0720@gmail.com>
2021-07-09 03:05:58 +0900

    feat: 홈화면 배경 및 타이틀 퍼블리싱

--- components/home/Title.tsx
+++ components/home/Title.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { ReactElement } from 'react';
 
-function Title() {
-  return <h1>Home</h1>;
+function Title(): ReactElement {
+  return (
+    <TitleWrap>
+      <div className="bgImage">
+        <p className="title">
+          Learn Korean through your
+          <br />
+          favorite K-Pop artists and songs!
+        </p>
+      </div>
+    </TitleWrap>
+  );
 }
+
+const TitleWrap = styled.div`
+  .bgImage {
+    /* background-size: cover; */
+    /* height: 440px; */
+    /* margin-top: 0;
+    margin-right: 0;
+    margin-left: 0; */
+    background: url(/assets/images/homeBackground.svg) no-repeat;
+    width: 100vw;
+    height: 435px;
+  }
+
+  .title {
+    padding-top: 132px;
+    text-align: center;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
+    line-height: 60px;
+    white-space: nowrap;
+    color: #ffffff;
+    font-size: 40px;
+    font-weight: bold;
+    font-style: normal;
+  }
+`;
 
 export default Title;
