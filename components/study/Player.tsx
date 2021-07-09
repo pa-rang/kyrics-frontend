559c9ea4ed1fcd07c29c8e2244789dd1c8d2286a
nahee-park <jessica396@ewhain.net>
2021-07-09 17:42:13 +0900

    feat: 리플레이 기능 구현

--- components/study/Player.tsx
+++ components/study/Player.tsx
@@ -9,9 +9,12 @@ interface PlayerProps {
   handleSeekTime: (e: any) => void;
   totalTime: number;
   handleVolumeChange: (e: any) => void;
-  handleBackTime: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  handleForwardTime: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  handleBackTime: () => void;
+  handleForwardTime: () => void;
   currentTime: number;
+  handleLoop: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  loop: boolean;
+  isMessageOpened: boolean;
 }
 function Player({
   playing,
@@ -23,12 +26,15 @@ function Player({
   handleBackTime,
   handleForwardTime,
   currentTime,
+  handleLoop,
+  loop,
+  isMessageOpened,
 }: PlayerProps): ReactElement {
   const title = '앨범 제목';
   const singer = '가수';
 
   return (
-    <PlayerWrapper>
+    <PlayerWrapper isMessageOpened={isMessageOpened}>
       <img className="player-album" src="assets/images/exampleImg.svg" alt="albumImage" />
       <div className="player-custom">
         <div className="player-custom__title">
@@ -55,7 +61,7 @@ function Player({
           <div className="player-custom__lastcontrol__volume">
             <button className="player-custom__lastcontrol__volume__btn"></button>
             <input
-              className="player-custom__lastcontrol__volume__bar"
+              className="player-custom__lastcontrol__volume__bar hover-to-display"
               type="range"
               min={0}
               max={100}
@@ -64,8 +70,19 @@ function Player({
             />
           </div>
           <div className="player-custom__lastcontrol__replay">
-            <button className="player-custom__lastcontrol__replay__btn"></button>
-            <button className="player-custom__lastcontrol__replay__onoff">on</button>
+            <button
+              className="player-custom__lastcontrol__replay__btn"
+              onClick={handleLoop}
+            ></button>
+            {loop ? (
+              <button className="player-custom__lastcontrol__replay__onoff click-to-display">
+                on
+              </button>
+            ) : (
+              <button className="player-custom__lastcontrol__replay__onoff click-to-display">
+                off
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -76,39 +93,54 @@ function Player({
 
 export default Player;
 
-const PlayerWrapper = styled.div`
+const PlayerWrapper = styled.div<{ isMessageOpened: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   background: url('assets/images/playerBackground.svg') no-repeat 0 0;
   width: 100%;
   height: 263px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
 
   button {
-    border: 0;
     outline: 0;
+    border: 0;
     cursor: pointer;
   }
   /* Chrome */
   input[type='range']::-webkit-slider-thumb {
     -webkit-appearance: none;
-    height: 16px;
-    width: 16px;
     border-radius: 50%;
     background: #ffffff;
     cursor: pointer;
+    width: 16px;
+    height: 16px;
   }
 
   input[type='range']::-ms-fill-lower {
-    background: #2a6495;
     border: 0.2px solid #010101;
     border-radius: 2.6px;
     box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #2a6495;
   }
 
   input[type='range']:focus {
     outline: none; /* input range에 포커스 되었을 경우 기본 블러처리 제거 */
   }
+  @keyframes fadeinout {
+    0% {
+      visibility: hidden;
+      opacity: 0;
+    }
+    50% {
+      visibility: visible;
+      opacity: 1;
+    }
+    100% {
+      visibility: hidden;
+      opacity: 0;
+    }
+  }
+
   .player-album {
     width: 160px;
     height: 160px;
@@ -117,52 +149,52 @@ const PlayerWrapper = styled.div`
     display: flex;
     flex-direction: column;
     &__title {
-      font-family: Noto Sans;
-      font-style: normal;
-      font-weight: bold;
-      font-size: 24px;
+      margin-bottom: 41px;
       text-align: center;
       color: #ffffff;
-      margin-bottom: 41px;
+      font-family: Noto Sans;
+      font-size: 24px;
+      font-weight: bold;
+      font-style: normal;
     }
     &__progressbar {
       -webkit-appearance: none;
+      margin-bottom: 20px;
+      border-radius: 10px;
+      background-color: #9d9d9d;
       width: 612px;
       height: 3px;
-      background-color: #9d9d9d;
-      border-radius: 10px;
-      margin-bottom: 20px;
     }
     &__time {
       display: flex;
       justify-content: space-between;
       width: 636px;
       &__current {
-        font-family: Roboto;
-        font-size: 12px;
         line-height: 14px;
         color: #e1e1e1;
+        font-family: Roboto;
+        font-size: 12px;
       }
       &__end {
-        font-family: Roboto;
-        font-size: 12px;
         line-height: 14px;
         color: #e1e1e1;
+        font-family: Roboto;
+        font-size: 12px;
       }
     }
     &__control {
       display: flex;
-      justify-content: center;
       align-items: center;
+      justify-content: center;
       &__back {
         background: url('assets/icons/backIcon.svg') no-repeat 0 0;
         width: 17px;
         height: 21px;
       }
       &__playBtn {
-        background: url('assets/icons/playIcon.svg') no-repeat 0 0;
-        margin-left: 75px;
         margin-right: 75px;
+        margin-left: 75px;
+        background: url('assets/icons/playIcon.svg') no-repeat 0 0;
         width: 31px;
         height: 31px;
       }
@@ -176,11 +208,11 @@ const PlayerWrapper = styled.div`
       display: flex;
       justify-content: space-between;
       &__volume {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
         width: 116.01px;
         height: 25px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
 
         &__btn {
           background: url('assets/icons/soundIcon.svg') no-repeat 0 0;
@@ -189,33 +221,35 @@ const PlayerWrapper = styled.div`
         }
         &__bar {
           -webkit-appearance: none;
+          border-radius: 10px;
           background-color: #9d9d9d;
           width: 76.01px;
-          border-radius: 10px;
           height: 3px;
         }
       }
       &__replay {
         display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
+        justify-content: center;
         &__btn {
+          margin-bottom: 10px;
           background: url('assets/icons/replayIcon.svg') no-repeat 0 0;
           width: 20px;
           height: 20px;
-          margin-bottom: 10px;
         }
         &__onoff {
-          font-family: Noto Sans KR;
-          font-weight: 500;
-          font-size: 12px;
-          line-height: 17px;
-          text-align: center;
-          color: #464646;
-          background: #f6f6f6;
+          visibility: hidden;
           opacity: 0.8;
           border-radius: 10px;
+          background: #f6f6f6;
+          text-align: center;
+          line-height: 17px;
+          color: #464646;
+          font-family: Noto Sans KR;
+          font-size: 12px;
+          font-weight: 500;
+          ${({ isMessageOpened }) => isMessageOpened && 'animation: fadeinout 2s'};
         }
       }
     }
--- pages/study.tsx
+++ pages/study.tsx
@@ -1,30 +1,39 @@
 import Lyrics from '@components/study/Lyrics';
 import Player from '@components/study/Player';
 import styled from '@emotion/styled';
-import React, { ReactElement, useRef, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 
 function Study(): ReactElement {
-  const [isPlay, setIsPlay] = useState<boolean>(false);
+  const [isPlay, setIsPlay] = useState<boolean>(true);
   const [currentTime, setCurrentTime] = useState<number>(0);
   const [volume, setVolume] = useState<number>(1);
   const [volumeBar, setVolumeBar] = useState<number>(0);
   const [loop, setLoop] = useState<boolean>(false);
   const [totalTime, setTotalTime] = useState<number>(0);
+  const [isMessageOpened, setIsMessageOpened] = useState<boolean>(false);
   const hostVideo = useRef();
 
   console.log(hostVideo.current);
+
+  useEffect(() => {
+    setTotalTime(hostVideo.current.getDuration());
+    console.log(hostVideo.current.getDuration());
+  }, [isPlay]);
+
   const handlePlay = () => {
     if (isPlay === true) {
-      setTotalTime(hostVideo.current.getDuration());
-      console.log(hostVideo.current.getDuration());
       setIsPlay(false);
     } else {
       setIsPlay(true);
     }
+    console.log(isPlay);
   };
 
   const handleLoop = () => {
+    setIsMessageOpened(true);
+    // 3초 뒤엔 다시 false로 바꿈
+    setTimeout(setIsMessageOpened, 2000, false);
     if (loop === true) {
       setLoop(false);
     } else {
@@ -40,40 +49,42 @@ function Study(): ReactElement {
   };
 
   // progress bar 제어 -> 원하는 시간 받으면 그 값으로 돌아가도록
-  const handleSeekTime = (e) => {
+  const handleSeekTime = (e: any) => {
     setCurrentTime(e.target.value);
     hostVideo.current.seekTo(e.target.value);
   };
 
-  const handleBackTime = (e) => {
+  const handleBackTime = () => {
     setCurrentTime(currentTime - 10);
     hostVideo.current.seekTo(currentTime - 10);
   };
 
-  const handleForwardTime = (e) => {
+  const handleForwardTime = () => {
     setCurrentTime(currentTime + 10);
     hostVideo.current.seekTo(currentTime + 10);
   };
 
-  const handleVolumeChange = (e) => {
+  const handleVolumeChange = (e: any) => {
     setVolume(e.target.value / 100);
     setVolumeBar(e.target.value);
   };
 
   return (
     <StudyWrapper>
-      <ReactPlayer
-        url="https://youtu.be/IHNzOHi8sJs"
-        playing={isPlay}
-        loop={loop}
-        controls={true}
-        light={true}
-        volume={volume}
-        ref={hostVideo}
-        width="650px"
-        height="400px"
-        onProgress={(e) => handleOnProgress(e)}
-      />
+      <div className="react-default-player">
+        <ReactPlayer
+          url="https://youtu.be/IHNzOHi8sJs"
+          playing={isPlay}
+          loop={loop}
+          controls={true}
+          light={true}
+          volume={volume}
+          ref={hostVideo}
+          width="650px"
+          height="400px"
+          onProgress={(e) => handleOnProgress(e)}
+        />
+      </div>
       <Player
         playing={isPlay}
         volume={volumeBar}
@@ -84,6 +95,9 @@ function Study(): ReactElement {
         handleBackTime={handleBackTime}
         handleForwardTime={handleForwardTime}
         currentTime={currentTime}
+        handleLoop={handleLoop}
+        loop={loop}
+        isMessageOpened={isMessageOpened}
       />
       <Lyrics />
     </StudyWrapper>
@@ -94,6 +108,9 @@ export default Study;
 
 const StudyWrapper = styled.div`
   .react-player__play-icon {
-    display: none;
+    /* display: none; */
+  }
+  .react-default-player {
+    /* display: none; */
   }
 `;
