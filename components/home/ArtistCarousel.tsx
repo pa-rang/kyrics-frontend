b344268d6bb29bd5a6489316433917ec699114b4
Danna Lee <daeunlee0720@gmail.com>
2021-07-14 04:40:16 +0900

    feat(home): 슬라이더 구현

--- components/home/ArtistCarousel.tsx
+++ components/home/ArtistCarousel.tsx
@@ -1,10 +1,15 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import styled from '@emotion/styled';
 import { mockClient } from 'lib/api';
 import React, { ReactElement } from 'react';
+import Slider, { Settings } from 'react-slick';
+import useSWR from 'swr';
+import { Artist } from 'types';
 
 import ArtistCard from './ArtistCard';
 import NewSongCard from './NewSongCard';
-
 function ArtistCarousel(): ReactElement {
   const { data } = useSWR<{ data: Artist[] }>('/artists', mockClient.get);
 
@@ -13,8 +18,10 @@ function ArtistCarousel(): ReactElement {
   const artists = data?.data;
 
   return (
-    <Wrap>
-      <ArtistCard
+    <>
+      <Styled.Root>
+        <Styled.InnerWrap>
+          <Slider {...sliderProps}>
             {artists?.map((artist: Artist) => (
               <ArtistCard
                 key={artist.id}
@@ -24,17 +31,82 @@ function ArtistCarousel(): ReactElement {
               />
             ))}
             <NewSongCard />
-    </Wrap>
+          </Slider>
+        </Styled.InnerWrap>
+      </Styled.Root>
+    </>
   );
 }
 
-const Wrap = styled.div`
-  display: flex;
-  position: absolute;
-  justify-content: center;
-  margin-top: -114px;
-  width: 100%;
-  height: 270px;
-`;
+const Styled = {
+  Root: styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: -114px;
+    height: 270px;
+  `,
+
+  InnerWrap: styled.div`
+    /* padding: 0 16px; */
+    width: 1200px;
+    .slick-list {
+      padding: 0 18px !important;
+    }
+
+    .slick-arrow {
+      &:before {
+        color: black;
+        /* font-size: 50px; */
+      }
+    }
+
+    /* .slick-track {
+      margin-right: auto;
+      margin-left: auto;
+    } */
+
+    @media (max-width: 1200px) {
+      width: 800px;
+      .slick-list {
+        padding: 0 18px !important;
+      }
+    }
+    @media (max-width: 1030px) {
+      width: 400px;
+
+      .slick-list {
+        padding: 0 20px !important;
+      }
+    }
+  `,
+};
+
+const sliderProps: Settings = {
+  arrows: true,
+  infinite: true,
+  // autoplay: true,
+  pauseOnHover: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  initialSlide: 0,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 1030,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 
 export default ArtistCarousel;
