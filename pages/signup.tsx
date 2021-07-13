3f139ea7fd6b53028d6eecf3e3b6777f9fbb9ea3
pa-rang <appdler96@kaist.ac.kr>
2021-07-14 01:47:49 +0900

    feat: add signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,14 +2,25 @@ import LinedTitle from '@components/signup/LinedTitle';
 import styled from '@emotion/styled';
 import { mainLogo } from '@public/assets';
 import React from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 function SignUp() {
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+
   return (
     <Styled.Root>
       <Styled.Contents>
         <Styled.Logo src={mainLogo.src} alt="kyrics" />
         <LinedTitle>Sign Up</LinedTitle>
-        <button>Sign up with Google</button>
+        <GoogleLogin
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+          buttonText="Sign up with Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </Styled.Contents>
     </Styled.Root>
   );
