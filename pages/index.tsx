519a7af2f92d05740ccf5edba1cc9273ed1cb17e
Danna Lee <daeunlee0720@gmail.com>
2021-07-14 04:41:18 +0900

    fix(home): 서버 연결 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
-import ArtistCarousel from '@components/home/ArtistCarousel';
 import Footer from '@components/common/Footer';
+import ArtistCarousel from '@components/home/ArtistCarousel';
+import styled from '@emotion/styled';
 import Title from 'components/home/Title';
-import { mockClient } from 'lib/api';
 import React from 'react';
-import useSWR from 'swr';
-import { Artist } from 'types';
 
 import Header from '../components/common/Header';
 
 function Home() {
-  const { data } = useSWR<{ data: Artist[] }>('/artists', mockClient.get);
-
-  console.log('data', data?.data);
-
   return (
     <>
       <Header isLoggedIn={true} />
