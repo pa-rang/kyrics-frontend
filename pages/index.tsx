4b5f9e12bc9cf67fe54a7c01f3c769ec53a04ae9
pa-rang <appdler96@kaist.ac.kr>
2021-07-16 00:49:49 +0900

    feat: add api client

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import Footer from '@components/common/Footer';
 import ArtistCarousel from '@components/home/ArtistCarousel';
 import styled from '@emotion/styled';
 import Title from 'components/home/Title';
+import { client } from 'lib/api';
 import React from 'react';
+import useSWR from 'swr';
 
 import Header from '../components/common/Header';
 
 function Home() {
+  const { data } = useSWR('/artists', client.get);
+
+  console.log('client data', data?.data);
+
   return (
     <>
       <Header isLoggedIn={true} />
