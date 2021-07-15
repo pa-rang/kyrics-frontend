6ff148e0541c9b0a7b680c7a52affa74eeb3d692
pa-rang <appdler96@kaist.ac.kr>
2021-07-15 21:55:36 +0900

    style: 홈화면 메인 카피 반응형 스타일 적용

--- components/home/Title.tsx
+++ components/home/Title.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
-import React, { ReactElement } from 'react';
+import React from 'react';
 
-function Title(): ReactElement {
+function Title() {
   return (
     <TitleWrap>
       <div className="bgImage">
@@ -27,12 +27,16 @@ const TitleWrap = styled.div`
     padding-top: 132px;
     text-align: center;
     text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
-    line-height: 60px;
+    line-height: 1.5;
     white-space: nowrap;
     color: #ffffff;
     font-size: 40px;
     font-weight: bold;
     font-style: normal;
+
+    @media (max-width: 768px) {
+      font-size: 16px;
+    }
   }
 `;
 
