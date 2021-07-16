e4ff0eb78df6619306e0f27e1e58c196d55bc873
nahee-park <jessica396@ewhain.net>
2021-07-16 17:00:45 +0900

    feat: 플레이어 서버연결

--- components/study/Player.tsx
+++ components/study/Player.tsx
@@ -1,10 +1,17 @@
 import PlayerBtns from '@components/study/PlayerBtns';
 import styled from '@emotion/styled';
+import { client } from 'lib/api';
 import React, { ReactElement } from 'react';
+import useSWR from 'swr';
 import { PlayerProps } from 'types';
 
 import PlayController from './PlayController';
 function Player({ handleSeekTime, handleBackTime, handleForwardTime }: PlayerProps): ReactElement {
+  const id = 1;
+  const { data } = useSWR(`/song/${id}`, client.get);
+
+  console.log('>>', data);
+
   return (
     <PlayerWrapper>
       <img className="player-album" src="assets/images/exampleImg.svg" alt="albumImage" />
--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
 export const mockClient = axios.create({ baseURL: 'http://localhost:3005' });
+export const client = axios.create({ baseURL: 'https://kyricserver.com' });
