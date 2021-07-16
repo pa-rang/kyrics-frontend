3565b74050be68580883c2dd7f3e016a22b39f8b
pa-rang <appdler96@kaist.ac.kr>
2021-07-16 23:24:39 +0900

    style: login-email page layout

---
+++ components/login/RegisterEmailInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegisterEmailInput() {
+  return <div>RegisterEmailInput</div>;
+}
+
+export default RegisterEmailInput;
---
+++ pages/login/email.tsx
@@ -0,0 +1,54 @@
+import LoginLayout from '@components/login/LoginLayout';
+import RegisterEmailInput from '@components/login/RegisterEmailInput';
+import styled from '@emotion/styled';
+import { colors } from 'lib/constants/colors';
+import { clickable } from 'lib/mixin';
+import React from 'react';
+
+function LoginEmailPage() {
+  return (
+    <LoginLayout>
+      <Styled.Copy>
+        Get the latest from Kyrics
+        <br /> by adding your email address!
+      </Styled.Copy>
+      <RegisterEmailInput />
+      <Styled.SkipButton>Skip and continue</Styled.SkipButton>
+    </LoginLayout>
+  );
+}
+
+export default LoginEmailPage;
+
+const Styled = {
+  Copy: styled.h2`
+    margin-bottom: 32px;
+    text-align: center;
+    line-height: 120%;
+    color: #464646;
+    font-size: 24px;
+    font-weight: bold;
+
+    @media (max-width: 768px) {
+      font-size: 14px;
+    }
+  `,
+
+  SkipButton: styled.button`
+    transition: all 0.2s ease-in-out;
+    margin-top: 32px;
+    border: none;
+    border-radius: 3px;
+    box-shadow: 3px 3px 7px 4px rgba(98, 98, 98, 0.09);
+    background: #fefcfe;
+    padding: 12px 44px;
+    color: ${colors.gray3};
+    font-size: 16px;
+    font-weight: 500;
+    ${clickable}
+
+    &:hover {
+      box-shadow: 3px 3px 7px 4px rgba(98, 98, 98, 0.15);
+    }
+  `,
+};
