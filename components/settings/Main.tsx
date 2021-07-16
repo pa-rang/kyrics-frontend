ff214034c7a7170bee1b3c8eaf9d16e1ba0be2bd
gytks4 <gytks4@naver.com>
2021-07-16 11:49:10 +0900

    feat(settings): 이메일 수정 모달 스타일 구현

--- components/settings/Main.tsx
+++ components/settings/Main.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { mockClient } from 'lib/api';
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 
 interface IUserData {
@@ -11,6 +11,7 @@ interface IUserData {
 }
 
 function Main() {
+  const [isModalOpened, setIsModalOpened] = useState(false);
   const { data } = useSWR<{ data: IUserData }>('/userdata', mockClient.get);
   const userData = data?.data;
 
@@ -18,8 +19,8 @@ function Main() {
     <Styled.Root>
       <Styled.Blank></Styled.Blank>
       <Styled.Container>
-        <img src={userData?.profileImageUrl} alt="profile" />
-        {/* <img src="/assets/images/profileExample.svg" alt="profile" /> */}
+        {/* <img src={userData?.profileImageUrl} alt="profile" /> */}
+        <img src="/assets/images/profileExample.svg" alt="profile" />
         <Styled.Name>{userData?.name}</Styled.Name>
         <Styled.Email>{userData?.email}</Styled.Email>
         <Styled.Desc>
@@ -28,13 +29,37 @@ function Main() {
         </Styled.Desc>
         <Styled.Edit>
           {userData?.email}
-          <img src="/assets/icons/editIcon.svg" alt="edit" />
+          <img
+            src="/assets/icons/editIcon.svg"
+            alt="edit"
+            onClick={() => setIsModalOpened(true)}
+            aria-hidden="true"
+          />
         </Styled.Edit>
         <Styled.Delete>
           <img src="/assets/icons/deleteIcon.svg" alt="delete" />
           <span>Delete Account</span>
         </Styled.Delete>
       </Styled.Container>
+      {isModalOpened && (
+        <Styled.ModalWrapper>
+          <Styled.Modal>
+            <Styled.ModalHeader></Styled.ModalHeader>
+            <Styled.ModalMain>
+              <Styled.ModalText>
+                Insert new email address to get latest from Kyrics
+              </Styled.ModalText>
+              <input type="text" defaultValue={userData?.email} />
+              <Styled.ModalButton>
+                <button onClick={() => setIsModalOpened(false)} aria-hidden="true">
+                  Cancel
+                </button>
+                <button>Save</button>
+              </Styled.ModalButton>
+            </Styled.ModalMain>
+          </Styled.Modal>
+        </Styled.ModalWrapper>
+      )}
     </Styled.Root>
   );
 }
@@ -76,6 +101,8 @@ const Styled = {
         margin-top: 50px;
         margin-bottom: 16px;
         background-color: #f8fafc;
+        width: 68px;
+        height: 68px;
       }
     }
     @media (max-width: 768px) {
@@ -144,6 +171,76 @@ const Styled = {
     font-weight: 500;
     & > img {
       margin-right: 8px;
+    }
+  `,
+  ModalWrapper: styled.div`
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.8);
+    width: 100%;
+    height: 100%;
+  `,
+  Modal: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 40%;
+    height: 210px;
+  `,
+  ModalHeader: styled.div`
+    border-radius: 10px 10px 0 0;
+    background-color: #6465f4;
+    width: 100%;
+    height: 16px;
+  `,
+  ModalMain: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 0 0 10px 10px;
+    background-color: #ffffff;
+    width: 100%;
+    height: 194px;
+    color: #464646;
+    input {
+      margin-bottom: 20px;
+      outline: 0;
+      border: 1px solid rgba(100, 101, 244, 0.86);
+      border-radius: 5px;
+      padding: 0 30px;
+      height: 39px;
+      text-align: center;
+      color: #464646;
+      font-size: 20px;
+      font-weight: 500;
+    }
+  `,
+  ModalText: styled.div`
+    margin-top: 30px;
+    margin-bottom: 21px;
+    text-align: center;
+    font-size: 20px;
+  `,
+
+  ModalButton: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 170px;
+    button {
+      outline: 0;
+      border: 0;
+      border-radius: 5px;
+      background-color: #9d9d9d;
+      width: 70px;
+      height: 27px;
+      color: #ffffff;
+      font-size: 12px;
     }
   `,
 };
