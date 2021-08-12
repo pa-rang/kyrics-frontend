b6530d373f6e167fba49ce23cf9b26abbb3250ab
pa-rang <appdler96@kaist.ac.kr>
2021-08-12 21:09:26 +0900

    HOTFIX: baseURL 임시로 변경

--- lib/api.ts
+++ lib/api.ts
@@ -15,12 +15,14 @@ export interface KyricsSWRResponse<T> {
 export const mockClient = axios.create({ baseURL: 'http://localhost:3005' });
 
 export const client = axios.create({
-  baseURL: 'https://kyricserver.com',
+  baseURL: 'http://kyrics-env.eba-kez2mzcm.ap-northeast-2.elasticbeanstalk.com/',
+  // baseURL: 'https://kyricserver.com',
   headers: {
     'x-access-token': isServer || localStorage.getItem('userToken'),
   },
 });
 
 export const clientWithoutToken = axios.create({
-  baseURL: 'https://kyricserver.com',
+  baseURL: 'http://kyrics-env.eba-kez2mzcm.ap-northeast-2.elasticbeanstalk.com/',
+  // baseURL: 'https://kyricserver.com',
 });
