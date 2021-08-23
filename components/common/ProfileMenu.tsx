8e2b1d8bc4dbefdaef33ff9a72c1e97d135d1718
gytks4 <gytks4@naver.com>
2021-08-24 04:18:04 +0900

    feat: profileMenu mobile dropdown 구현

--- components/common/ProfileMenu.tsx
+++ components/common/ProfileMenu.tsx
@@ -1,16 +1,16 @@
 import styled from '@emotion/styled';
-import { useMobile } from 'hooks/useMobile';
+import { usePhone } from 'hooks/useMobile';
 import { useRouter } from 'next/router';
 import React, { ReactElement } from 'react';
 
 function ProfileMenu(): ReactElement {
   const router = useRouter();
-  const isMobile = useMobile();
+  const isMobile = usePhone() ? 'Mobile' : '';
 
   return (
     <Wrap>
       <button className="option" onClick={() => router.push('/mypage/settings')}>
-        <img className="option__icon" src="/assets/icons/IcSetting.svg" alt=""></img>
+        <img className="option__icon" src={`/assets/icons/Ic${isMobile}Setting.svg`} alt=""></img>
         <p className="option__label">Account Settings</p>
         <div className="option__border"></div>
       </button>
@@ -23,7 +23,7 @@ function ProfileMenu(): ReactElement {
           })
         }
       >
-        <img className="option__icon" src="/assets/icons/IcMySong.svg" alt=""></img>
+        <img className="option__icon" src={`/assets/icons/Ic${isMobile}MySong.svg`} alt=""></img>
         <p className="option__label">My Songs</p>
         <div className="option__border"></div>
       </button>
@@ -36,7 +36,7 @@ function ProfileMenu(): ReactElement {
           })
         }
       >
-        <img className="option__icon" src="/assets/icons/IcMyVoca.svg" alt=""></img>
+        <img className="option__icon" src={`/assets/icons/Ic${isMobile}MyVoca.svg`} alt=""></img>
         <p className="option__label">My Vocab</p>
       </button>
     </Wrap>
@@ -50,6 +50,7 @@ const Wrap = styled.div`
   right: 0px;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   z-index: 1000;
   border-radius: 16px;
   box-shadow: 3px 3px 7px 4px rgba(98, 98, 98, 0.12);
@@ -64,8 +65,8 @@ const Wrap = styled.div`
     border: none;
     background: transparent;
     cursor: pointer;
-    /* width: 204px; */
-    /* height: 30px; */
+    width: 204px;
+    height: 30px;
 
     &:hover {
       filter: brightness(0) saturate(100%) invert(33%) sepia(48%) saturate(2268%) hue-rotate(222deg)
@@ -74,22 +75,26 @@ const Wrap = styled.div`
 
     &__icon {
       margin-left: 10px;
-      width: 19px;
-      height: 19px;
+      /* width: 19px;
+      height: 19px; */
       @media (max-width: 415px) {
+        transform: translateY(1px);
         margin: 0;
+        /* width: 10px;
+        height: 10px; */
       }
     }
 
     &__label {
       margin-left: 12px;
-      line-height: 22px;
+      /* line-height: 22px; */
       color: #9d9d9d;
       font-size: 16px;
       font-weight: 500;
       font-style: normal;
       @media (max-width: 415px) {
-        margin: 0;
+        margin-left: 4px;
+        /* line-height: 10px; */
         font-size: 7px;
       }
     }
@@ -97,18 +102,26 @@ const Wrap = styled.div`
     &__border {
       opacity: 0.53;
       margin-top: 7px;
-      background: #6465f4;
-      width: 50px;
+      background: #e1e1e1;
+      width: 204px;
       height: 1px;
       @media (max-width: 415px) {
-        margin: 0;
+        margin-top: 2;
+        width: 106px;
       }
+    }
+    @media (max-width: 415px) {
+      margin-top: 4px;
+      width: 104px;
+      height: 20px;
     }
   }
 
   @media (max-width: 415px) {
-    width: 110px;
-    height: 80px;
+    top: 30px;
+    padding-top: 4px;
+    width: 122px;
+    height: 81px;
   }
 `;
 
