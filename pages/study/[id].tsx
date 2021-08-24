944527ee61083a6435aeed9f318bd5274fe3988b
gytks4 <gytks4@naver.com>
2021-08-24 11:30:54 +0900

    chore: mute 제거

--- pages/study/[id].tsx
+++ pages/study/[id].tsx
@@ -63,7 +63,7 @@ function Study(): ReactElement {
   }, [currentTime]);
 
   useEffect(() => {
-    setMute(false);
+    // setMute(false);
     if (host !== null) {
       setTotalTime(Math.floor(host.getDuration()));
     }
@@ -163,7 +163,7 @@ function Study(): ReactElement {
     };
   }, []);
 
-  const [mute, setMute] = useState(true);
+  // const [mute, setMute] = useState(true);
 
   if (!id) {
     return <div>Loading...</div>;
@@ -188,7 +188,7 @@ function Study(): ReactElement {
             onPlay={() => setIsPlay(true)}
             onPause={() => setIsPlay(false)}
             progressInterval={100}
-            muted={mute}
+            // muted={mute}
             config={{
               youtube: {
                 playerVars: {
